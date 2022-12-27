b48cd758dea5fed66d681f46266de747b7681bf8
nakamura-snowflake-handson <121269227+nakamura-snowflake-handson@users.noreply.github.com>
2022-12-27 14:01:57 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-from urlib.error import URLError 
+from urllib.error import URLError 
 
 my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 my_fruit_list = my_fruit_list.set_index('Fruit')
